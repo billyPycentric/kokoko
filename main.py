48cef789dab5d364347a1234b1cd851a9fb588e9
abongile <a.billy@pycentric.net>
2025-06-04 19:52:05 +0200

    Testing done

--- main.py
+++ main.py
@@ -1,42 +1,6 @@
-import os
-import sys  # unused import
-
-
-def foo():
-    password = "123456"  # hardcoded secret
-    x = 42  # unused variable
-    for i in range(5):
-        print("Bad loop")
-        print("Bad loop")  # duplicate line
-    try:
-        1 / 0
-    except:
-        pass  # empty catch block
-
-
-def Bar():
-    print("MixedCase function name")  # naming convention violation
-
-
-def baz():
-    return
-    print("This is unreachable")  # unreachable code
-
-
-def foo():  # duplicate function name
-    print("Function redefined")
-
-    # bad.py
-
-
-def bad_function():
-    x = 1 / 0  # division by zero
-    print("Hello" + 1)  # type error
-    unused_var = 123  # unused variable
-    names = 32
+def get_name():
+    return "Hello Billy"
 
 
 if __name__ == "__main__":
-    foo()
-    Bar()
-    baz()
+    get_name()
